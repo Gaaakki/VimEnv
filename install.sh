12643467cdaee3f31feb4372281bf00bea35ad64
renying <15501100731@163.com>
2020-08-21 17:01:09 +0800

    comment install vim8

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-cd ~/VimEnv/vim
 
-sudo yum -y install ncurses-devel lua-devel python-devel perl-devel ruby-devel perl-ExtUtils-Embed
-make distclean 
-./configure --enable-multibyte --with-tlib=ncurses --enable-cscope --with-features=huge --enable-luainterp --with-lua-prefix=/usr/local/ --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ --enable-perlinterp
-sudo make 
-sudo make install
+# Install vim8
+# cd ~/VimEnv/vim
+# sudo yum -y install ncurses-devel lua-devel python-devel perl-devel ruby-devel perl-ExtUtils-Embed
+# make distclean 
+# ./configure --enable-multibyte --with-tlib=ncurses --enable-cscope --with-features=huge --enable-luainterp --with-lua-prefix=/usr/local/ --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu/ --enable-perlinterp
+# sudo make 
+# sudo make install
 
 cd ~
 rm .tmux.conf
@@ -15,5 +16,5 @@ ln -s ~/VimEnv/.vim/vimrc .vimrc
 rm -rf .vim
 ln -s ~/VimEnv/.vim .vim
 
-# Install Vundle
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+# Install Vim-Plug
+# curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
